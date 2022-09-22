d66ef9c16e865ad225475bb33fcb4f5afc71315b
Karrar Qasim <Karawr1996@gmail.com>
2022-09-21 21:39:16 -0600

    show unfinished player class

---
+++ player.js
@@ -0,0 +1,16 @@
+class Player {
+  constructor(player) {
+    this.name = player.name;
+    this.token = player.token;
+    this.wins = 0;
+    this.choice = "";
+    this.choiceImage = "";
+  }
+
+  takeTurn(playerChoice) {
+    this.choice = playerChoice;
+  }
+
+  winGame() {
+    this.wins++;
+  }
