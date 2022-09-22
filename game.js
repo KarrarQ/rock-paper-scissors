43e3433a59e2ffeea69a37b5714d2b4425a6e4fd
Karrar Qasim <Karawr1996@gmail.com>
2022-09-22 16:18:24 -0600

    Added human and computer choices functionalty

--- game.js
+++ game.js
@@ -11,3 +11,30 @@ class Game {
     var computer = new Player({name: "Computer", token: "🤖"});
     this.players.push(computer);
   }
+
+  setPlayerChoice(player, playerChoice) {
+    this.players[player].takeTurn(playerChoice);
+  }
+
+  playMatch() {
+    var humanChoice = this.players[0].choice;
+    var computerChoice = this.players[1].choice;
+    this.compareChoices(humanChoice, computerChoice);
+  }
+
+  compareChoices(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      this.winner = {name: "Draw"};
+    } else if (humanChoice === "Rock") {
+      this.evaluateChoices(computerChoice, "Scissors", "Lizard");
+    } else if (humanChoice === "Scissors") {
+      this.evaluateChoices(computerChoice, "Paper", "Lizard");
+    } else if (humanChoice === "Paper") {
+      this.evaluateChoices(computerChoice, "Rock", "Alien");
+    } else if (humanChoice === "Lizard") {
+      this.evaluateChoices(computerChoice, "Paper", "Alien");
+    } else if (humanChoice === "Alien") {
+      this.evaluateChoices(computerChoice, "Rock", "Scissors");
+    }
+  }
+}
